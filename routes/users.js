7e601e9f980eb36133675f96d172ea7c72a6db56
Joseph Donovan <joeydonovan4@gmail.com>
2018-06-12 13:46:07 -0400

    Create user routes

---
+++ routes/users.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var userController = require('../controllers/userController');
+
+router.get('/', (req, res) => {
+    userController.findAllUsers(req, res);
+});
+
+router.get('/:id', (req, res) => {
+    userController.findUserById(req, res);
+});
+
+router.post('/', (req, res) => {
+    userController.createUser(req, res);
+});
+
+module.exports = router;
